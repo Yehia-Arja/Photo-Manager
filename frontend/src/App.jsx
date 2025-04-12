16a7dbfb2e400bdc896f70d64b0c54c67a92b6cb
Yehia Arja <yehia.al.arja12@gmail.com>
2025-04-12 21:21:14 +0300

    fixed the home page ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home"
 import "./App.css"
 import RecentlyDeleted from "./pages/RecentlyDeleted"
+import Nav from "./components/NavBar"
 
 function App() {
   
   return (
+    <Nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/deleted" element={<RecentlyDeleted />} />
       </Routes>
+    </ Nav>
   )
 }
 
